835379ebd9b4dfdd13e1e9f277145dbb00301b43
Wade-Philander <wadephilander7@gmail.com>
2021-07-27 10:50:55 +0200

    vid 11

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('',views.home, name='home'),
     path('products/',views.products, name='products'),
-    path('customers',views.customers, name='customers'),
+    path('customers/<str:pk_test>/',views.customers, name='customers'),
+    path('create_order/',views.createOrder, name='create_order'),
+    path('update_order/<str:pk>/',views.updateOrder, name='update_order'),
+    path('delete_order/<str:pk>/',views.deleteOrder, name='delete_order')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Product, Order, Customer
+from .forms import OrderForm
+from django.forms import inlineformset_factory
 
 # Create your views here.
 
@@ -38,4 +40,49 @@ def customers(request, pk_test):
     context = {'customers': customers, 'orders':orders, 'order_count':order_count}
 
     return render(request,'accounts/customers.html', context)
- 
+
+def createOrder(request, pk):
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product','status'), extra=10)
+    customer = Customer.objects.get(id=pk)
+    formset = OrderFormSet(queryset=Order.objects.none(), instance=customer)
+    #form = OrderForm(initial={'customer': customer})
+    if request.method == 'POST':
+        #print('Printing POST:', request.POST)
+        #form = OrderForm(request.POST)
+        formset = OrderFormSet(request.POST, instance=customer)
+        if formset.is_valid():
+            formset.save()
+            return redirect('/')
+
+    context ={'formset': formset}
+
+
+    return render(request,'accounts/order_form.html', context)
+
+def updateOrder(request, pk):
+
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+
+    if request.method == 'POST':
+        print('Printing POST:', request.POST)
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form, 'order': order}
+
+    return render(request, 'accounts/order_form.html', context)
+
+
+def deleteOrder(request, pk):
+
+    order = Order.objects.get(id=pk)
+
+    if request.method == 'POST':
+        order.delete()
+        return redirect('/')
+    context = {'item':order}
+
+    return render(request, 'accounts/delete.html', context)
