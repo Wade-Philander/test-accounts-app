8c0714bd063a837aeaebc7de14eff9447648f0dc
Wade-Philander <wadephilander7@gmail.com>
2021-08-24 16:07:28 +0200

    test

--- function_test/test_accounts.py
+++ function_test/test_accounts.py
@@ -4,6 +4,9 @@ from accounts.models import *
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.urls import reverse
 import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 # driver = webdriver.Chrome("/home/wade/Desktop/work/selenium/chromedriver")
 
@@ -46,8 +49,34 @@ class TestCMRApp(StaticLiveServerTestCase):
         submit.click()
         print("clicking")
 
-    def TestCustomerRegisterForm(self):
-        self.username = WebDriver(self.browser.find_element_by_id('id_username'))
+    # def TestCustomerRegisterForm(self):
+    #     self.username = WebDriver(self.browser.find_element_by_id('id_username'))
         
-        self.email = WebDriver(self.browser.find_element_by_id('id_password1'))
-        
+    #     self.email = WebDriver(self.browser.find_element_by_id('id_password1'))
+
+
+    def test_register(self):
+            self.regLink = self.browser.find_element_by_class_name("ml-2")
+            
+            self.regLink.click()
+            try:
+                self.username = WebDriverWait(self.browser, 50).until(EC.presence_of_element_located((By.ID, "id_username")))
+                self.username.send_keys("dxfxxdcffxfxfxf")
+                self.email = self.browser.find_element_by_id("id_email")
+                self.email.send_keys("yghvtfvghvguvfhhvgv@gmail.com")
+                self.password1 = self.browser.find_element_by_id("id_password1")
+                self.password1.send_keys("ftfftufctfyvcgfdyvfc1!")
+                self.password2 = self.browser.find_element_by_id("id_password2")
+                self.password2.send_keys("ftfftufctfyvcgfdyvfc1!")
+                
+            except Exception as err:
+                print(err)
+                self.browser.quit()
+            
+            try:
+                self.registerButton = self.browser.find_element_by_class_name("btn")
+                self.registerButton.click()
+            except Exception as err:
+                print(err)
+                
+            time.sleep(2000)
